1c56665d1510895d171a41caef78cb0a637623c4
Johan Hultén <121808363+Zindriana@users.noreply.github.com>
2024-02-02 16:38:46 +0100

    Add files via upload

--- src/main/java/org/example/phoneduck/controller/ChatController.java
+++ src/main/java/org/example/phoneduck/controller/ChatController.java
@@ -1,6 +1,7 @@
 package org.example.phoneduck.controller;
 
 import org.example.phoneduck.model.ChatRoomModel;
+import org.example.phoneduck.model.MessageModel;
 import org.example.phoneduck.service.ChatService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -34,4 +35,10 @@ public class ChatController {
         chatService.updateRoom(title, newChatRoom.getTitle());
     }
 
+    @PutMapping("/{title}")
+    public void createMessage(@PathVariable String title, @RequestBody MessageModel message){
+        MessageModel newMessage = chatService.createMessage(title, message);
+
+
+    }
 }
--- src/main/java/org/example/phoneduck/service/ChatService.java
+++ src/main/java/org/example/phoneduck/service/ChatService.java
@@ -1,7 +1,9 @@
 package org.example.phoneduck.service;
 
 import org.example.phoneduck.model.ChatRoomModel;
+import org.example.phoneduck.model.MessageModel;
 import org.example.phoneduck.repository.ChatRepository;
+import org.example.phoneduck.repository.MessageRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,9 +14,14 @@ public class ChatService {
 
     @Autowired
     private ChatRepository chatRepository;
+    private MessageRepository messageRepository;
 
     public List<ChatRoomModel> getAllRooms(){
        return chatRepository.findAll();
+    }
+
+    public ChatRoomModel getIdByTitle(String title){
+        return chatRepository.findByTitle(title);
     }
 
     public String createRoom(ChatRoomModel chatRoomModel){
@@ -39,5 +46,12 @@ public class ChatService {
             room.setTitle(newTitle);
         }
         return chatRepository.save(room);
+    }
+
+    public MessageModel createMessage(String title, MessageModel messageModel){
+        MessageModel message = messageModel;
+        ChatRoomModel room = chatRepository.findByTitle(title);
+        message.setRoomId(room.getId());
+        return messageRepository.save(message);
     }
 }
